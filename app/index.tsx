c173c5ac1db729085ae0e6b0f74512a924fdc22d
deshan <deshan@boffosys.com>
2024-03-22 22:21:01 +0530

    basic

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
 
-export default function Home() {
+export default function StartPage() {
   return (
-    <View className="bg-slate-400 pt-10">
-      <Text className="text-3xl text-center">Home</Text>
+    <View className="flex-1 justify-center items-center">
+      <ActivityIndicator size={'large'} color={'#007aff'} />
      </View>
   );
 }
