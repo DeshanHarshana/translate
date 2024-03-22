77e933c566c2545e845d053032434ff99b35278e
deshan <deshan@boffosys.com>
2024-03-22 11:54:20 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Home() {
+  return (
+    <View className="bg-slate-400 pt-10">
+      <Text className="text-3xl text-center">Home</Text>
+     </View>
+  );
+}
